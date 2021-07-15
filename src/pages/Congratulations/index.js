6cc40126fcf625dddc675a932cabea8ee885f553
Mariya Kardash <mariya.kardash2@gmail.com>
2021-07-15 23:10:34 +0300

    create congrat page

--- src/pages/Congratulations/index.js
+++ src/pages/Congratulations/index.js
@@ -1,5 +1,21 @@
+import { getScoreFromLocaleStorage } from "../../localStorage";
+import { Header } from "../../components/Header";
+import { H3 } from "../../components/styled";
+
 export function CongratulationsPage() {
+    let scoreRecords = getScoreFromLocaleStorage();
+
+    let lastRecord = scoreRecords[scoreRecords.length - 1];
+
     return(
-    <h1>Congratulations page</h1>
+    <>
+        <Header/>
+        <h1>{lastRecord.user.firstName}, CONGRATULATIONS!</h1>
+        <H3>You win!!!</H3>
+        <div>Difficulty: {lastRecord.mode.difficulty}</div>
+        <div>Card theme: {lastRecord.mode.cardTheme}</div>
+        <div>Time: {lastRecord.time.split(' ')[1]}</div>
+        <div>Steps: {lastRecord.score.step}</div>
+    </>
     )
 }
