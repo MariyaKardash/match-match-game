2c597564a0d56167bb178bfcaa96b6804a58c3b2
Mariya Kardash <mariya.kardash2@gmail.com>
2021-07-19 10:12:30 +0300

    add summer pictures

--- src/arrays.js
+++ src/arrays.js
@@ -1,8 +1,10 @@
-export const flowers = ['https://image.freepik.com/free-photo/flowers-white-background_24972-6.jpg', 
-'https://media.istockphoto.com/photos/flowers-composition-yellow-and-purple-flowers-on-white-background-picture-id1127776025?k=6&m=1127776025&s=612x612&w=0&h=zaxF-PTAaXiz12ZAFESCuYHIuGNcKALVBtzEdIApnMo=',
-'https://i.pinimg.com/originals/f7/0f/e7/f70fe71c70e86d37b3dcfd4c47588491.jpg',
-'https://image.shutterstock.com/image-photo/orange-pink-yellow-cosmos-flowers-260nw-1041253204.jpg',
-'https://st4.depositphotos.com/21087722/23085/i/1600/depositphotos_230854498-stock-photo-beautiful-watercolor-flowers-white-background.jpg',
-'https://images.unsplash.com/photo-1585232350675-7c1ffea3f5ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80',
-'https://thumbs.dreamstime.com/b/beautiful-bright-gerbera-flower-white-background-top-view-147756469.jpg',
-'https://c4.wallpaperflare.com/wallpaper/301/449/198/flowers-white-background-beautiful-blue-flowers-wallpaper-preview.jpg']
+export const flowers = [
+  "/images/summer/summer1.png",
+  "/images/summer/summer2.png",
+  "/images/summer/summer3.png",
+  "/images/summer/summer4.png",
+  "/images/summer/summer5.png",
+  "/images/summer/summer6.png",
+  "/images/summer/summer7.png",
+  "/images/summer/summer8.png",
+];
